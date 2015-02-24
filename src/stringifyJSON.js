758ff23924bb08cdc49d63a162a5fb82386d7c6e
LanceFulton <lsfaccounts@gmail.com>
2015-02-24 17:16:12 -0600

    Cleaned up syntax.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,59 +6,59 @@
 
 
 var stringifyJSON = function(obj) {
-  var result = "";
-  if (typeof obj === "number"){
-  	result += obj.toString();
+	var result = "";
+	if (typeof obj === "number"){
+		result += obj.toString();
   	return result;
-  }
-  if (obj === null){
-  	result += "null";
-  	return result;
-  }
-  if (obj === true){
-  	result += "true";
-  	return result;
-  } 
-  if (obj === false){
-  	result += "false";
-  	return result;
-  }
-  if (typeof obj === "string"){
-  	result += '"' + obj + '"';
-  	return result;
-  }
-  
-  function ifArray(array, i){
-  	if (i === 0){
-  		arrayResult += "[";
-  		if (typeof array[i] === "string"){
-  			arrayResult += '"' + array[i] + '"';
-  		}
-  		if (typeof array[i] === "number"){
-  			arrayResult += array[i];
-  		}
-  		timesRun++;
-  	}
-  	if (i !== 0){
-  		if (typeof array[i] === "string"){
-  			arrayResult += ',"' + array[i] + '"';
-  		}
-  		if (typeof array[i] === "number"){
-  			arrayResult += "," + array[i];
-  		}
-  	}
-  	if (i === array.length){
-  		arrayResult += "]";
-  		return arrayResult;
-  	}
-  	return ifArray(array, i+1);
-  };
+	}
+	if (obj === null){
+		result += "null";
+		return result;
+	}
+	if (obj === true){
+		result += "true";
+		return result;
+	} 
+	if (obj === false){
+		result += "false";
+		return result;
+	}
+	if (typeof obj === "string"){
+		result += '"' + obj + '"';
+		return result;
+	}
 
-  if (obj.constructor === Array){
-  	var timesRun = 0;
-  	var arrayResult = "";
-  	result += ifArray(obj, 0);
-  	return result;
-  }
+	function ifArray(array, i){
+	  	if (i === 0){
+	  		arrayResult += "[";
+	  		if (typeof array[i] === "string"){
+	  			arrayResult += '"' + array[i] + '"';
+	  		}
+	  		if (typeof array[i] === "number"){
+	  			arrayResult += array[i];
+	  		}
+	  		timesRun++;
+	  	}
+	  	if (i !== 0){
+	  		if (typeof array[i] === "string"){
+	  			arrayResult += ',"' + array[i] + '"';
+	  		}
+	  		if (typeof array[i] === "number"){
+	  			arrayResult += "," + array[i];
+	  		}
+	  	}
+	  	if (i === array.length){
+	  		arrayResult += "]";
+	  		return arrayResult;
+	  	}
+	  	return ifArray(array, i+1);
+	};
+
+	if (obj.constructor === Array){
+		var timesRun = 0;
+		var arrayResult = "";
+		result += ifArray(obj, 0);
+		return result;
+	}
 };
 
